e65de146da7086d3c75042a8b162ad41da8adb96
Karan Sanjeev <yo@alphaman.me>
2020-09-23 14:30:54 +0530

    Basic working example

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello');
+import * as express from 'express';
+import add from '@simple-monorepo/common';
+
+const app: express.Application = express();
+
+app.get('/add/:numberOne/:numberTwo', (req, res) => res.json({
+    sum: add(parseInt(req.params.numberOne), parseInt(req.params.numberTwo))
+}));
+
+app.listen(8080, () => console.log('Listening on port 8080'));
